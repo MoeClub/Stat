ffe9b6ef205784dce7487faad60d043e60dbb79f
MoeClub <32381827+MoeClub@users.noreply.github.com>
2023-11-08 13:51:31 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+case `uname -m` in aarch64|arm64) ARCH="arm64";; x86_64|amd64) ARCH="amd64";; *) ARCH="";; esac
+[ -n "$ARCH" ] || exit 1
+
+Type="${1:-StatClient}"
+[ "${Type}" != "StatClient" ] && [ "${Type}" != "StatServer" ] && exit 1
+
+URL="https://github.com/MoeClub/Stat/raw/main"
+StatDir="/etc/stat"
+
+systemctl stop "${Type}.service" >/dev/null 2>&1
+systemctl disable "${Type}.service" >/dev/null 2>&1
+rm -rf "${StatDir}"
+
+wget --no-check-certificate -4 -qO- "${URL}/${Type}/${ARCH}/linux/${Type}"
+wget --no-check-certificate -4 -qO- "${URL}/${Type}/${Type}.service"
+[ "${Type}" == "StatServer" ] && wget --no-check-certificate -4 -qO- "${URL}/index.html"
+
+chmod -R 777 "${StatDir}"
+
+systemctl daemon-reload >/dev/null 2>&1
+systemctl enable "${Type}.service" >/dev/null 2>&1
+systemctl start "${Type}.service" >/dev/null 2>&1
+systemctl status "${Type}.service"
+
+
