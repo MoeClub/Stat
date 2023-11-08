41eae0fc7bf1d67579a483767b3598a568a4a9bd
MoeClub <32381827+MoeClub@users.noreply.github.com>
2023-11-08 13:59:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ systemctl stop "${Type}.service" >/dev/null 2>&1
 systemctl disable "${Type}.service" >/dev/null 2>&1
 rm -rf "${StatDir}"
 
+mkdir -p "${StatDir}"
 wget --no-check-certificate -4 -qO "${StatDir}/${Type}" "${URL}/${Type}/${ARCH}/linux/${Type}"
 wget --no-check-certificate -4 -qO "${StatDir}/${Type}.service" "${URL}/${Type}/${Type}.service"
 [ "${Type}" == "StatServer" ] && wget --no-check-certificate -4 -qO "${StatDir}/index.html" "${URL}/${Type}/index.html"
